51eab2eb10849108aa07b32cf94fa1ab645454e4
Argens HFA <54584533+ArgensJCJr@users.noreply.github.com>
2025-04-10 11:14:34 -0300

    Add files via upload

--- Angulo/Angulo.java
+++ Angulo/Angulo.java
@@ -2,26 +2,39 @@ package Angulo;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.*;
 
 public class Angulo extends JPanel implements ActionListener {
-	static int LARGURA = 1200;
-	static int ALTURA = 800;
-	static int MARGEM = 150;
+	int LARGURA = 1400;
+	int ALTURA = 1000;
+	static int MARGEM = 30;
 	
-	int tamanhoMax = 35;
+	static int CONTADORMAX = 30;
+	static int INTERVALOMAX = 20;
+	
+	int tamanhoMax = 65;
+	int tamanhoMin = 35;
+	int vidaMax = 600;
 	double direcaoPadrao = 90;
-	int intensidadePadrao = 7;
+	int intensidadePadrao = 15;
 	double vetorPolar[] = {direcaoPadrao, intensidadePadrao};
 	double vetorCartesiana[] = {0,0};
 	
-	Bola bolinha1;
+	int explosaoMax = 150;
+	int varVida = 4;
+	static Color corExplosaoInicial = new Color(0xff0000);
+	static Color corExplosaoFinal = new Color(0xffff5f);
+	float varCor[] = {0,0,0};
+	
+	ArrayList<Bola> Bolas = new ArrayList();
+	ArrayList<Explosao> Explosoes = new ArrayList<>();
 	Random rand = new Random();
 	Timer timer;
 	
-	int tempContador = 1;
+	int intervalo = INTERVALOMAX;
 	boolean novoVetor = true;
 	
 	public void vetorVeloc(double direcao, double intensidade) {
@@ -33,27 +46,50 @@ public class Angulo extends JPanel implements ActionListener {
 	
 	Angulo(){
 		this.setPreferredSize(new Dimension(LARGURA, ALTURA));
-		this.setBackground(new Color(0x351010));
+		this.setBackground(new Color(0x3f3f6f));
 		geraBola();
-		timer = new Timer(10, this);
+		calculaVarCor();
+		timer = new Timer(30, this);
 		timer.start();
 	}
 	
+	private void calculaVarCor() {
+		float corIni[] = corExplosaoInicial.getColorComponents(null);
+		float corFim[] = corExplosaoFinal.getColorComponents(null);
+		int razao = explosaoMax / varVida;
+		for(int i = 0; i<3;i++) {
+			varCor [i] = (corFim[i] - corIni[i]) / razao;
+		}
+	}
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		draw(g);
 	}
 	
 	public void draw(Graphics g) {
+		LARGURA = this.getWidth();
+		ALTURA = this.getHeight();
 		
-		float cores[] = bolinha1.cor.getComponents(null);
-		g.setColor(new Color(cores[0], cores[1], cores[2], ((float)bolinha1.vida/255)));
-//		g.setColor(new Color(255, 100, 0, 255));
-		g.fillOval((int)bolinha1.x, (int)bolinha1.y, bolinha1.tamanho, bolinha1.tamanho);
+		for (Bola bolinha:Bolas) {
+			float cores[] = bolinha.cor.getComponents(null);
+			g.setColor(new Color(cores[0], cores[1], cores[2], ((float)bolinha.vida/vidaMax)));
+			g.fillOval((int)bolinha.x, (int)bolinha.y, bolinha.tamanho, bolinha.tamanho);
+		}
+		if (Explosoes.size()>0) {
+			for(Explosao explo:Explosoes) {
+				float cores[] = explo.cor.getColorComponents(null);
+				g.setColor(new Color(cores[0], cores[1], cores[2], ((float)explo.vida/explosaoMax)));
+				g.fillOval((int)explo.x, (int)explo.y, explo.tamanho, explo.tamanho);
+			}
+		}
+		
 	}
 	
 	public void geraBola() {
-		bolinha1 = new Bola();
+		Bola bolinha = new Bola();
+		Bolas.add(bolinha);
+		intervalo = INTERVALOMAX;
 	}
 	
 	public class Bola {
@@ -62,57 +98,127 @@ public class Angulo extends JPanel implements ActionListener {
 		double direcao;
 		double intensidade;
 		int tamanho;
-		int vida = 255;
+		int vida = vidaMax;
 		Color cor;
 		
 		Bola(){
 			this.x = 0;
 			this.y = rand.nextInt(ALTURA - tamanhoMax - 2*MARGEM) + MARGEM;
-			this.direcao = rand.nextInt(90) + 45;
+			this.direcao = rand.nextInt((int)direcaoPadrao) + direcaoPadrao/2;
 			this.intensidade = rand.nextInt(intensidadePadrao) + intensidadePadrao/2;
 			vetorVeloc(this.direcao, this.intensidade);
 			this.velocX = vetorCartesiana[0];
 			this.velocY = vetorCartesiana[1];
-			this.tamanho = tamanhoMax;	
-			this.cor = new Color(125, 30, 200);
+			this.tamanho = rand.nextInt(tamanhoMax - tamanhoMin) + tamanhoMin;	
+			int corBasica = 150;
+			int corVariante = 255-corBasica;
+			this.cor = new Color(rand.nextInt(corBasica)+corVariante,rand.nextInt(corBasica)+corVariante,rand.nextInt(corBasica)+corVariante);
+		}
+	}
+	
+	public class Explosao {
+		int x, y;
+		int tamanho = 1;
+		int vida = explosaoMax;
+		Color cor = corExplosaoInicial;
+		
+		Explosao(int x, int y){
+			this.x = x;
+			this.y = y;	
 		}
 	}
 	
 	public void move() {
-		bolinha1.x += bolinha1.velocX;
-		bolinha1.y += bolinha1.velocY;
-		bolinha1.vida -= 1;
+		for (Bola bolinha:Bolas) {
+			bolinha.x += bolinha.velocX;
+			bolinha.y += bolinha.velocY;
+			bolinha.vida -= 1;
+		}
 		checaColisao();
 	}
 	
 	public void checaColisao() {
-		if((bolinha1.y + bolinha1.tamanho) > ALTURA || bolinha1.y < 0){
-			bolinha1.direcao = 180 - bolinha1.direcao;
-			novoVetor = true;
+		for (Bola bolinha:Bolas) {
+			if((bolinha.y + bolinha.tamanho) > ALTURA || bolinha.y < 0){
+				bolinha.direcao = 180 - bolinha.direcao;
+				novoVetor = true;
+			}
+			if(bolinha.x < 0 || (bolinha.x + bolinha.tamanho) > LARGURA ){
+				bolinha.direcao = 360 - bolinha.direcao;
+				novoVetor = true;
+			}
+			if (novoVetor) {
+				vetorVeloc(bolinha.direcao, bolinha.intensidade);	
+				bolinha.velocX = vetorCartesiana[0];
+				bolinha.velocY = vetorCartesiana[1];
+			}
 		}
-		if(bolinha1.x < 0 || (bolinha1.x + bolinha1.tamanho) > LARGURA ){
-			bolinha1.direcao = 360 - bolinha1.direcao;
-			novoVetor = true;
+		for (int i=0; i< Bolas.size(); i++) {
+			if(Bolas.get(i).vida < 10) {
+				Bolas.remove(i);
+				geraBola();
+			}
 		}
-		if (novoVetor) {
-			vetorVeloc(bolinha1.direcao, bolinha1.intensidade);	
-			bolinha1.velocX = vetorCartesiana[0];
-			bolinha1.velocY = vetorCartesiana[1];
+		if(Bolas.size()>1) {
+			for (int i=1; i< Bolas.size(); i++) {
+				int tamA = Bolas.get(i).tamanho/2;
+				double xA = Bolas.get(i).x + tamA;
+				double yA = Bolas.get(i).y + tamA;
+				for (int j = 0; j < i; j++) {
+					int tamB = Bolas.get(j).tamanho/2;
+					double xB = Bolas.get(j).x + tamB;
+					double yB = Bolas.get(j).y + tamB;
+					double distancia = Math.sqrt(Math.pow((xA - xB), 2) + Math.pow((yA - yB), 2));
+					if ((tamA + tamB - distancia) > 1) {
+						Bolas.remove(i);
+						Bolas.remove(j);
+						geraExplosao(xA, yA);
+						geraExplosao(xB, yB);
+						break;
+					}
+				}
+			}
 		}
-		if(bolinha1.vida < 10) {
-			tempContador = 0;
-		}
+		
 	}
 
+	private void geraExplosao(double x, double y) {
+		Explosao explosao = new Explosao((int)x, (int)y);
+		Explosoes.add(explosao);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if(tempContador == 0) {
-			geraBola();
-			tempContador ++;
+		int contador = Bolas.size();
+		if (contador < CONTADORMAX) {
+			intervalo --;
+			if (intervalo == 0) {
+				geraBola();
+			}
 		}
 		move();
+		explodindo();
 		repaint();		
+	}
+
+	private void explodindo() {
+		if (Explosoes.size()>0) {
+			for(int i = 0; i < Explosoes.size(); i++) {
+				Explosao explo = Explosoes.get(i);
+				explo.x -= 1;
+				explo.y -= 1;
+				explo.tamanho += 2;
+				explo.vida -= varVida;
+				
+				float cores[] = explo.cor.getColorComponents(null);
+				explo.cor = new Color(cores[0]+varCor[0], cores[1]+varCor[1], cores[2]+varCor[2]);
+				
+				if(explo.tamanho>tamanhoMax) {
+					Explosoes.remove(i);
+				}
+			}
+		}
+		
 	}
 	
 }
--- Angulo/AppAngulo.java
+++ Angulo/AppAngulo.java
@@ -4,9 +4,11 @@ import javax.swing.JFrame;
 
 public class AppAngulo {
 	
+	static int www;
+	
 	public static void main(String[] args) {
 		
-		JFrame Fundo = new JFrame("Tiro em angulos");
+		JFrame Fundo = new JFrame("Meteroros");
 		Angulo painel = new Angulo();
 		Fundo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Fundo.setFocusable(true);
@@ -15,7 +17,6 @@ public class AppAngulo {
 		Fundo.pack();
 		Fundo.requestFocus();
 		Fundo.setLocationRelativeTo(null);
-		
 	}
 
 }
